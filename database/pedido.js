70d7cb201666da852ca6ff092fad71f885b84838
luiztesta <luiz.eduardo.testa.santos@gmail.com>
2023-04-28 09:49:59 -0300

    add model pedido

---
+++ database/pedido.js
@@ -0,0 +1,27 @@
+const { connection } = require("../database/database");
+const { DataTypes } = require("sequelize");
+const Cliente = require("./cliente");
+const Produto = require("./produto");
+
+// documento(tabela task) e propriedades do documento(titulo, descricao, status)
+const Pedido = connection.define(
+    "pedido",
+    {
+        codigo: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+        },
+        quantidade: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+        }
+    }
+);
+
+Cliente.hasMany(Pedido, { foreignKey: { type: DataTypes.UUID } }, { onDelete: "CASCADE" });
+Pedido.belongsTo(Cliente);
+
+Produto.hasMany(Pedido, { foreignKey: { type: DataTypes.UUID } }, { onDelete: "CASCADE" });
+Pedido.belongsTo(Produto);
+
+module.exports = Pedido;
